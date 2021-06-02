35c3913850aba2314272a240a575f9b0559458cf
ggaltar <gcorralesj@gmail.com>
2021-06-02 15:40:49 -0600

    Agregando coropletas

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Mapa Leaflet
 var mapa = L.map('mapid').setView([9.8, -84.25], 8.5);
 
-// Definición de capas base
+// Definición de capas base de tesela
 var capa_osm = L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?', 
   {
@@ -10,7 +10,7 @@ var capa_osm = L.tileLayer(
   }
 ).addTo(mapa);	
 
-// Otra capa base
+// Otra capa base de tesela
 var CartoDB_Voyager = L.tileLayer(
 	'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
 	{
@@ -65,13 +65,63 @@ $.getJSON("https://raw.githubusercontent.com/ggaltar/danos_red_vial/main/capas/r
 $.getJSON("https://raw.githubusercontent.com/ggaltar/danos_red_vial/main/capas/danos_wgs84.geojson", function(geodata) {
   var capa_danos = L.geoJson(geodata, {
     style: function(feature) {
-	  return {'color': "green", 'weight': 2.5, 'fillOpacity': 0.0}
+	  return {'color': "green", 'weight': 2.5, 'fillOpacity': 1.0}
     },
     onEachFeature: function(feature, layer) {
-      var popupText = "<strong>Severidad</strong>: " + feature.properties.SEVERIDAD;
+      var popupText = "<strong>Elemento</strong>: " + feature.properties.elemento + "<br>" + "<strong>Tipo de daño</strong>: " + feature.properties.tipo + "<br>" + "<strong>Severidad</strong>: " + feature.properties.severidad;
       layer.bindPopup(popupText);
     }			
   }).addTo(mapa);
 
   control_capas.addOverlay(capa_danos, 'Daños reportados');
+});
+
+// Capa WMS
+var capa_cantones = L.tileLayer.wms('https://geos.snitcr.go.cr/be/IGN_5/wms?', {
+	  layers: 'limitecantonal_5k',
+	  format: 'image/png',
+	  //transparent: true
+	  opacity: 0.4
+}).addTo(mapa);
+
+control_capas.addOverlay(capa_cantones, 'Cantones');
+
+// Capa de coropletas de cantidad de daños reportados por zona de conservación
+$.getJSON('https://raw.githubusercontent.com/ggaltar/danos_red_vial/main/capas/zonas_conservacion_wgs84.geojson', function (geojson) {
+  var capa_zonas_coropletas = L.choropleth(geojson, {
+	  valueProperty: 'cantidad',
+	  scale: ['yellow', 'brown'],
+	  steps: 5,
+	  mode: 'q',
+	  style: {
+	    color: '#fff',
+	    weight: 2,
+	    fillOpacity: 0.7
+	  },
+	  onEachFeature: function (feature, layer) {
+	    layer.bindPopup('Zona de conservación: ' + feature.properties.Zona2 + '<br>' + 'Cantidad de daños: ' + feature.properties.cantidad.toLocaleString())
+	  }
+  }).addTo(mapa);
+  control_capas.addOverlay(capa_zonas_coropletas, 'Cantidad de daños reportados por zona de conservación');	
+
+  // Leyenda de la capa de coropletas
+  var leyenda = L.control({ position: 'bottomright' })
+  leyenda.onAdd = function (mapa) {
+    var div = L.DomUtil.create('div', 'info legend')
+    var limits = capa_zonas_coropletas.options.limits
+    var colors = capa_zonas_coropletas.options.colors
+    var labels = []
+
+    // Add min & max
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+			<div class="max">' + limits[limits.length - 1] + '</div></div>'
+
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+    })
+
+    div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+    return div
+  }
+  leyenda.addTo(mapa)
 });
